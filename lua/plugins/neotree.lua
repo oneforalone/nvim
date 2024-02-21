446e2ea56364a3b3dd05c001e30a1ba398859fea
oneforalone <oneforalone@proton.me>
2024-02-21 15:31:22 +0800

    add: options for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,6 +9,20 @@ return {
   keys = {
     { "<c-b>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+          '.DS_Store',
+        }
+      }
+    }
+  },
   config = function()
     require("neo-tree").setup()
   end,
